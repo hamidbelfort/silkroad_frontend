d675adf0a1ef6fb0e11583f7ff2e5c4cb15f1dfe
Hamid <h.sorkhroo@gmail.com>
2025-04-12 16:24:48 +0330

    dashboard layout bug fixed

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,16 +1,22 @@
 "use client";
 
-import SidebarWrapper from "./components/dashboard/sideBarWrapper";
+import SidebarWrapper from "./components/dashboard/SideBarWrapper";
 import { ReactNode } from "react";
 
-const DashboardLayout = ({ children }: { children: ReactNode }) => {
+const DashboardLayout = ({
+  children,
+}: {
+  children: ReactNode;
+}) => {
   return (
     <div className="flex min-h-screen w-full bg-muted text-foreground">
       {/* Sidebar */}
       <SidebarWrapper />
 
       {/* Main content */}
-      <main className="flex-1 p-4 md:p-6 lg:p-8">{children}</main>
+      <main className="flex-1 p-4 md:p-6 lg:p-8">
+        {children}
+      </main>
     </div>
   );
 };
