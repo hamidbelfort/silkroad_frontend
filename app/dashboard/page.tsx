c2517ba51269e7c834f02932e2d347efbeb097d1
Hamid <h.sorkhroo@gmail.com>
2025-04-16 13:24:25 +0330

    update : bugs fixed again

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,14 +1,13 @@
 "use client";
 import React from "react";
 import { CustomerDashboardCards } from "./components/dashboard/customer/dashboardCards";
-import { useAuthStore } from "@/store/authStore";
+//import { useAuthStore } from "@/store/authStore";
 const Dashboard = () => {
-  const { role } = useAuthStore();
-  const normalizedRole = role?.toLowerCase();
-  console.log(role);
   return (
     <div>
-      <h1 className="text-2xl font-bold">Welcome to Your Dashboard 🎉</h1>
+      <h1 className="text-2xl font-bold">
+        Welcome to Your Dashboard 🎉
+      </h1>
       <p className="mt-2 text-muted-foreground">
         This is your dashboard overview.
         <CustomerDashboardCards />
